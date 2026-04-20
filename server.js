a7e5ef55375bd786560135dcc4c5ad3d3017a547
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-04-20 14:35:18 +0200

    first launch

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${chalk.blue(`http://localhost:${PORT}`)}`);
 });
